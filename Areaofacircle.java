71285502539d394d6a2a7a7cac886da1e159b017
Sameer Paudel <paudelsameer888@gmail.com>
2025-03-09 06:44:57 +0545

    Added some files

--- Areaofacircle.java
+++ Areaofacircle.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
-public class Areaofacircle{
-public static void main(String[] args){
-float PI = 3.1415926f;
-Scanner input = new Scanner(System.in);
-System.out.println("Enter the radius of the circle: ");
-float radius = input.nextFloat();
-input.close();
-float area = PI * radius * radius;
-System.out.println("The area is : "+area);
-}
+
+public class Areaofacircle {
+    public static void main(String[] args) {
+        float PI = 3.1415926f;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the radius of the circle: ");
+        float radius = input.nextFloat();
+        input.close();
+        float area = PI * radius * radius;
+        System.out.println("The area is : " + area);
+    }
 }
